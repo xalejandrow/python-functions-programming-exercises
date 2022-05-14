9e85a21ec35e5e70ca1e258cca5bcedcb5bbf133
Alew <xalejandrow@gmail.com>
2022-05-14 23:04:41 +0000

    ej05

--- .learn/resets/05-Defining-vs-Calling-a-function/app.py
+++ .learn/resets/05-Defining-vs-Calling-a-function/app.py
@@ -1,6 +1,4 @@
 # Define the function called "multi" that expects 2 parameters:
-def multi(num1, num2):
-    return num1 * num2
 
 # don't edit anything below this line
 return_value = multi(7,53812212)
