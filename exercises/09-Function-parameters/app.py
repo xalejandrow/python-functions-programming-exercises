f5cc21dcb68a0524e1aaf4a1caa74c847b935466
Alew <xalejandrow@gmail.com>
2022-05-14 23:21:06 +0000

    ej09

--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,6 +1,9 @@
 # Your code goes here:
-def render_person(param):
-    return param
+#def render_person(param):
+#    return param
+def render_person(name, born, eyes, age, gender):
+    text =  name +" is a "+ str(age) +" years old "+gender+" born in "+born+" with "+eyes+" eyes"
+    return text
 
 
 # Do not edit below this line
